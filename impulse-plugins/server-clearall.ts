23c5c824353d606f5629d82a09ef3f4dec91bc95
PrinceSky-Git <princeskygithub@gmail.com>
2025-09-05 18:38:51 +0530

    Create server-clearall.ts

---
+++ impulse-plugins/server-clearall.ts
@@ -0,0 +1,51 @@
+/* clearall & globalclearall commands */
+
+function clearRooms(rooms: Room[], user: User): string[] {
+  const clearedRooms: string[] = [];
+  for (const room of rooms) {
+    if (!room) continue;
+    if (room.log.log) {
+      room.log.log.length = 0;
+    }
+    const userIds = Object.keys(room.users) as ID[];
+    for (const userId of userIds) {
+      const userObj = Users.get(userId);
+      if (userObj?.connections?.length) {
+        for (const connection of userObj.connections) {
+          userObj.leaveRoom(room, connection);
+        }
+      }
+    }
+    clearedRooms.push(room.id);
+    setTimeout(() => {
+      for (const userId of userIds) {
+        const userObj = Users.get(userId);
+        if (userObj?.connections?.length) {
+          for (const connection of userObj.connections) {
+            userObj.joinRoom(room, connection);
+          }
+        }
+      }
+    }, 1000);
+  }
+  return clearedRooms;
+}
+
+export const commands: Chat.ChatCommands = {
+  clearall(target: string, room: ChatRoom | null, user: User): void {
+	  if (room?.battle) {
+		  return this.sendReply("You cannot clearall in battle rooms.");
+	  }
+    if (!room) {
+      return this.errorReply("This command requires a room.");
+	 }
+	  this.checkCan('roommod', null, room);
+    clearRooms([room], user);
+  },
+
+  globalclearall(target: string, room: ChatRoom | null, user: User): void {
+	  this.checkCan('globalban', null, room);
+
+    const roomsToClear = Rooms.global.chatRooms.filter((chatRoom): chatRoom is Room => !!chatRoom && !chatRoom.battle);
+    const clearedRooms = clearRooms(roomsToClear, user);
+  },
